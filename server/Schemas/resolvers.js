1ebc18f640c879ff67b3ffa8c50664e882bbe119
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-02-10 09:49:46 -0500

    I have created a resolver with Me Query to find user based on Id

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -3,3 +3,16 @@ const {AuthenticationError} = require('apollo-server-express');
 const{User}= require('../models');
 
 const {signToken}= require('../utils/auth');
+
+const resolvers={
+    Query{
+        me: async(parent,arsg,context)=>{
+            if(context.user){
+                const userData= await User.findOne({_id:context.user._id}).selext('-__v -password');
+
+                return userData;
+            }
+            throw new AuthenticationError('you are not Logged In')
+        }
+    }
+}
