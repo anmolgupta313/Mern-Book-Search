402b0d6808eb05375b19af6ee329cd8dba3ce37a
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-02-10 09:30:09 -0500

    I have created a folder for schemas and added a type def file mutation file and index.js file plus I have also installed the graphql and apollo server express in the server side

---
+++ server/Schemas/typeDef.js
@@ -0,0 +1,43 @@
+const {gql}= require('apollo-server-express');
+
+const typeDef= gql`
+type User{
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [bookSchema]
+}
+
+type Book {
+    bookId: String
+    authors:[String]
+    description: String
+    title: String
+    image:String
+    link:String
+}
+
+input booksInput{
+    bookId: String
+    authors:[String]
+    description: String
+    title: String
+    image:String
+    link:String
+}
+type Auth{
+    toke:ID!
+    user:User
+}
+
+type Query{
+    me:User
+}
+
+type Mutation{
+    
+}
+`
+
+module.exports = typeDef;
