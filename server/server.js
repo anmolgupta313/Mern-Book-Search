7847a4bc11a56f3ab220bc0a8705388c17545258
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-02-15 19:21:34 -0500

    I have changed the path in server.js

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build"))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/"))
 })
 
 db.once('open', () => {
